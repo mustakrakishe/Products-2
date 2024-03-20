3251cb33594c2bff8d55c0f0572cf630b7491c58
mustakrakishe <mustakrkish@gmail.com>
2024-03-20 20:53:08 +0200

    Create register page

---
+++ laravel/resources/views/auth/register.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title', 'Register')
+
+@section('content')
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-50">Register</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="#" method="POST">
+            @csrf
+            <div>
+                <x-input id="email" name="email" type="email" required label="Email address"></x-input>
+            </div>
+            <div>
+                <x-input id="password" name="password" type="password" required label="Password"></x-input>
+            </div>
+            <div>
+                <x-input id="confirm-password" name="confirm-password" type="password" required label="Confirm password"></x-input>
+            </div>
+
+            <div>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
 
         <title>@yield('title')</title>
 
+        <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
@@ -21,6 +23,25 @@
             <div class="relative min-h-screen flex flex-col items-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                        <div class="flex lg:justify-center lg:col-start-2">
+                        </div>
+                        <nav class="-mx-3 flex flex-1 justify-end">
+                            <a
+                                href="{{ route('home') }}"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Home
+                            </a>
+                            @auth
+                            @else
+                                <a
+                                    href="{{ route('register') }}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Register
+                                </a>
+                            @endauth
+                        </nav>
                     </header>
 
                     <main class="mt-6 flex justify-center">
