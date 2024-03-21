e8b9f99f53f0541b7fcfe5b00628977c1b3eb6ae
mustakrakishe <mustakrkish@gmail.com>
2024-03-21 10:47:07 +0200

    Create web log out

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -33,6 +33,12 @@
                                 Home
                             </a>
                             @auth
+                                <a
+                                    href="{{ route('logout') }}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                    Log out
+                                </a>
                             @else
                                 <a
                                     href="{{ route('register') }}"
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\Auth\LogoutController;
 use App\Http\Controllers\Web\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,8 @@ Route::prefix('auth')->group(function () {
         Route::get('verify', 'getVerificationNoticePage')->name('verification.notice');
         Route::get('verify/{id}/{hash}', 'verify')->middleware('signed')->name('verification.verify');
     });
+
+    Route::get('logout', [LogoutController::class, 'logout'])
+        ->middleware('auth')
+        ->name('logout');
 });
