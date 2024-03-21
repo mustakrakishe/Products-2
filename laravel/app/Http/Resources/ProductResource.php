12af2cba1cc2f5c84023294611e42c8f45be4e4f
mustakrakishe <mustakrkish@gmail.com>
2024-03-21 16:33:31 +0200

    Create Product resource

---
+++ laravel/app/Http/Resources/ProductResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'title'      => $this->title,
+            'price'      => floatval($this->price),
+            'currency'   => new CurrencyResource($this->currency),
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+        ];
+    }
+}
