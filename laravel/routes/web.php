efa130660b6989a7d737bdb5afda23aef024ecfa
mustakrakishe <mustakrkish@gmail.com>
2024-03-22 10:26:40 +0200

    Move web product controller to web workspace

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\Web\Auth\LoginController;
 use App\Http\Controllers\Web\Auth\LogoutController;
 use App\Http\Controllers\Web\Auth\RegisterController;
 use App\Http\Controllers\Web\Auth\ResetPasswordController;
+use App\Http\Controllers\Web\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
