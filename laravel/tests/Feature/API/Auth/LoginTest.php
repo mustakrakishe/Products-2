ddfc063371f9727b87814232ec931c31f2ef9f6e
mustakrakishe <mustakrkish@gmail.com>
2024-03-26 00:20:28 +0200

    Create api login failed validation test

--- laravel/tests/Feature/API/Auth/LoginTest.php
+++ laravel/tests/Feature/API/Auth/LoginTest.php
@@ -103,4 +103,115 @@ class LoginTest extends TestCase
             ],
         ];
     }
+
+    #[DataProvider('invalidInputDataProvider')]
+    public function test_if_input_is_invalid_then_fails_validation(array $errors, array $input): void
+    {
+        $response = $this->post('api/auth/login', $input);
+
+        $response->assertUnprocessable();
+        $response->assertInvalid($errors);
+    }
+
+    public static function invalidInputDataProvider(): array
+    {
+        return [
+            'email_is_missing' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'password' => 'password',
+                ],
+            ],
+            'email_is_null' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'email'    => null,
+                    'password' => 'password',
+                ],
+            ],
+            'email_is_empty' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'email'    => '',
+                    'password' => 'password',
+                ],
+            ],
+            'email_has_wrong_format' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'email'    => 'user.example.com',
+                    'password' => 'password',
+                ],
+            ],
+            'email_has_wrong_data_type' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'email'    => 123,
+                    'password' => 'password',
+                ],
+            ],
+            'email_is_too_short' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'email'    => 'u@e',
+                    'password' => 'password',
+                ],
+            ],
+            'email_is_too_long' => [
+                'errors' => ['email'],
+                'input'   => [
+                    'email'    => str_repeat('a', 254).'@e',
+                    'password' => 'password',
+                ],
+            ],
+            'email_is_wrong' => [
+                'errors' => ['email', 'password'],
+                'input'   => [
+                    'email'    => 'wrong@example.cpm',
+                    'password' => 'password',
+                ],
+            ],
+            'password_is_missing' => [
+                'errors' => ['password'],
+                'input'   => [
+                    'email'    => 'user@example.com',
+                ],
+            ],
+            'password_is_null' => [
+                'errors' => ['password'],
+                'input'   => [
+                    'email'    => 'user@example.com',
+                    'password' => null,
+                ],
+            ],
+            'password_is_empty' => [
+                'errors' => ['password'],
+                'input'   => [
+                    'email'    => 'user@example.com',
+                    'password' => '',
+                ],
+            ],
+            'password_has_wrong_data_type' => [
+                'errors' => ['password'],
+                'input'   => [
+                    'email'    => 'user@example.com',
+                    'password' => 123,
+                ],
+            ],
+            'password_is_too_long' => [
+                'errors' => ['password'],
+                'input'   => [
+                    'email'    => 'user@example.com',
+                    'password' => str_repeat('a', 256),
+                ],
+            ],
+            'password_is_wrong' => [
+                'errors' => ['password', 'email'],
+                'input'   => [
+                    'email'    => 'user@example.com',
+                    'password' => 'wrong_password',
+                ],
+            ],
+        ];
+    }
 }
