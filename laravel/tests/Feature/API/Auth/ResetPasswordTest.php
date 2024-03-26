a737bf4e8af45c26ebab8effaa4f3b4b05167893
mustakrakishe <mustakrkish@gmail.com>
2024-03-26 20:56:19 +0200

    Create api password reset validation failure test

--- laravel/tests/Feature/API/Auth/ResetPasswordTest.php
+++ laravel/tests/Feature/API/Auth/ResetPasswordTest.php
@@ -100,4 +100,242 @@ class ResetPasswordTest extends TestCase
             ]],
         ];
     }
+
+    #[DataProvider('invalidPasswordResetDataProvider')]
+    public function test_if_input_is_invalid_then_fails_validation(string $invalid, callable $inputCallback): void
+    {
+        $user = User::factory()->create([
+            'email' => 'user@example.com',
+        ]);
+
+        $response = $this->post(
+            'api/auth/password/reset',
+            $inputCallback($user)
+        );
+
+        $response->assertUnprocessable();
+        $response->assertInvalid($invalid);
+    }
+
+    public static function invalidPasswordResetDataProvider(): array
+    {
+        return [
+            'token_is_missing' => [
+                'invalid' => 'token',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'token_is_null' => [
+                'invalid' => 'token',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => null,
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'token_is_empty' => [
+                'invalid' => 'token',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => '',
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'token_is_unexisted' => [
+                'invalid' => 'token',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => 'unexisted_token',
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'token_is_wrong' => [
+                'invalid' => 'token',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user).'1',
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_is_missing' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_is_null' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => null,
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_is_empty' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => '',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_has_wrong_format' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user.example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_has_wrong_data_type' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 123,
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_is_too_long' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => str_repeat('a', 254).'@a',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'email_is_unexisted' => [
+                'invalid' => 'email',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'unexisted@.example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'password_is_missing' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password_confirmation' => 'password',
+                    ];
+                },
+            ],
+            'password_is_null' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => null,
+                        'password_confirmation' => null,
+                    ];
+                },
+            ],
+            'password_is_empty' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => '',
+                        'password_confirmation' => '',
+                    ];
+                },
+            ],
+            'password_has_wrong_data_type' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => 123,
+                        'password_confirmation' => 123,
+                    ];
+                },
+            ],
+            'password_is_too_long' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => str_repeat('a', 256),
+                        'password_confirmation' => str_repeat('a', 256),
+                    ];
+                },
+            ],
+            'password_confirmation_is_missing' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                    ];
+                },
+            ],
+            'password_confirmation_is_null' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => null,
+                    ];
+                },
+            ],
+            'password_confirmation_is_different_form_password' => [
+                'invalid' => 'password',
+                'inputCallback' => function (User $user) {
+                    return [
+                        'token'                 => Password::createToken($user),
+                        'email'                 => 'user@example.com',
+                        'password'              => 'password',
+                        'password_confirmation' => 'password_confirmation',
+                    ];
+                },
+            ],
+        ];
+    }
 }
