158eeac6a523ee37e22f09c813a7fde1bdf557d1
mustakrakishe <mustakrkish@gmail.com>
2024-03-26 09:42:13 +0200

    Create api authorized logout test

--- laravel/tests/Feature/API/Auth/LogoutTest.php
+++ laravel/tests/Feature/API/Auth/LogoutTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\API\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -14,5 +15,26 @@ class LogoutTest extends TestCase
         $response = $this->post('api/auth/logout');
 
         $response->assertUnauthorized();
+    }
+
+    public function test_if_authorized_then_removes_current_token_and_returns_correct_response(): void
+    {
+        $user = User::factory()->create();
+        $currentToken = $user->createToken('Current');
+        $anotherToken = $user->createToken('Another');
+
+        $response = $this
+            ->withHeader(
+                'Authorization',
+                sprintf('Bearer %s', $currentToken->plainTextToken)
+            )
+            ->post('api/auth/logout');
+
+        $response->assertNoContent();
+        $this->assertCount(1, $user->tokens);
+        $this->assertEquals(
+            $anotherToken->accessToken->id,
+            $user->tokens()->first()->id
+        );
     }
 }
