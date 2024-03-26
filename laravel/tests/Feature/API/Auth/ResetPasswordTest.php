5ef5a8cfb99dc278cbfbeac8fe1dc7ecf708b646
mustakrakishe <mustakrkish@gmail.com>
2024-03-26 20:27:39 +0200

    Create api successful password reset test

--- laravel/tests/Feature/API/Auth/ResetPasswordTest.php
+++ laravel/tests/Feature/API/Auth/ResetPasswordTest.php
@@ -4,7 +4,10 @@ namespace Tests\Feature\API\Auth;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class ResetPasswordTest extends TestCase
@@ -25,5 +28,76 @@ class ResetPasswordTest extends TestCase
             ]);
 
         $response->assertForbidden();
+    }
+
+    #[DataProvider('validResetPasswordDataProvider')]
+    public function test_if_data_is_valid_then_update_db_and_returns_ok(array $input): void
+    {
+        $user = User::factory()->create([
+            'email'    => trim($input['email']),
+            'password' => 'old_password',
+        ]);
+
+        $response = $this->post('api/auth/password/reset', $input + [
+            'token' => Password::createToken($user),
+        ]);
+
+        $user->refresh();
+
+        $response->assertOk();
+        $this->assertDatabaseRecordsAreCorrect($user);
+    }
+
+    protected function assertDatabaseRecordsAreCorrect(User $user): void
+    {
+        $this->assertFalse(
+            DB::table('password_reset_tokens')
+                ->whereEmail($user->email)
+                ->exists()
+        );
+        $this->assertFalse(
+            Hash::check('old_password', $user->password)
+        );
+    }
+
+    public static function validResetPasswordDataProvider(): array
+    {
+        return [
+            'regular' => [[
+                'email'                 => 'user@example.com',
+                'password'              => 'password',
+                'password_confirmation' => 'password',
+            ]],
+            'email_is_trimless' => [[
+                'email'                 => ' user@example.com ',
+                'password'              => 'password',
+                'password_confirmation' => 'password',
+            ]],
+            'email_is_shortest' => [[
+                'email'                 => 'a@a',
+                'password'              => 'password',
+                'password_confirmation' => 'password',
+            ]],
+            'email_is_longest' => [[
+                'email'                 => str_repeat('a', 253).'@a',
+                'password'              => 'password',
+                'password_confirmation' => 'password',
+            ]],
+            'password_is_trimless' => [[
+                'email'                 => 'user@example.com',
+                'password'              => ' password ',
+                'password_confirmation' => ' password ',
+            ]],
+            'password_is_shortest' => [[
+                'email'                 => 'user@example.com',
+                'password'              => 'p',
+                'password_confirmation' => 'p',
+            ]],
+            'password_is_longest' => [[
+                'email'                 => 'user@example.com',
+                'password'              => str_repeat('a', 255),
+                'password_confirmation' => str_repeat('a', 255),
+            ]],
+        ];
     }
 }
