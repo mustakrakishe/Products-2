cd1f78dd2d8691157a314253c21038be977d6085
mustakrakishe <mustakrkish@gmail.com>
2024-03-28 09:25:20 +0200

    Create web registration test

--- laravel/tests/Feature/Web/Auth/RegisterTest.php
+++ laravel/tests/Feature/Web/Auth/RegisterTest.php
@@ -15,10 +15,11 @@ class RegisterTest extends TestCase
 {
     use RefreshDatabase;
 
-    #[DataProvider('validRegisterDataProvider')]
+    #[DataProvider('validInputDataProvider')]
     public function test_if_input_is_valid_then_creates_records_and_sends_email_and_returns_notice(array $input): void
     {
         Notification::fake();
+        VerifyEmail::createUrlUsing(null);
 
         $response = $this->post(route('register'), $input);
 
@@ -71,7 +72,7 @@ class RegisterTest extends TestCase
         return $request->hasValidSignature();
     }
 
-    public static function validRegisterDataProvider(): array
+    public static function validInputDataProvider(): array
     {
         return [
             'regular' => [[
@@ -134,6 +135,184 @@ class RegisterTest extends TestCase
                 'password'              => str_repeat('a', 255),
                 'password_confirmation' => str_repeat('a', 255),
             ]],
+        ];
+    }
+
+    #[DataProvider('invalidInputDataProvider')]
+    public function test_if_input_is_invalid_then_fails_validation(string $invalid, array $input): void
+    {
+        $response = $this->post(
+            route('register'),
+            $input,
+            ['HTTP_REFERER' => route('register')]
+        );
+
+        $response->assertFound();
+        $response->assertRedirectToRoute('register');
+        $response->assertInvalid($invalid);
+    }
+
+    public static function invalidInputDataProvider(): array
+    {
+        return [
+            'name_is_missing' => [
+                'invalid' => 'name',
+                'input'   => [
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'name_is_null' => [
+                'invalid' => 'name',
+                'input'   => [
+                    'name'                  => null,
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'name_is_empty' => [
+                'invalid' => 'name',
+                'input'   => [
+                    'name'                  => '',
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'name_has_wrong_data_type' => [
+                'invalid' => 'name',
+                'input'   => [
+                    'name'                  => 123,
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'name_is_too_long' => [
+                'invalid' => 'name',
+                'input'   => [
+                    'name'                  => str_repeat('a', 256),
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'email_is_missing' => [
+                'invalid' => 'email',
+                'input' => [
+                    'name'                  => 'New User',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'email_is_null' => [
+                'invalid' => 'email',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => null,
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'email_is_empty' => [
+                'invalid' => 'email',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => '',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'email_has_wrong_data_type' => [
+                'invalid' => 'email',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 123,
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'email_has_wrong_format' => [
+                'invalid' => 'email',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user.example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'email_is_too_long' => [
+                'invalid' => 'email',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'u@'.str_repeat('a', 254),
+                    'password'              => 'password',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'password_is_missing' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password_confirmation' => 'password',
+                ],
+            ],
+            'password_is_null' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password'              => null,
+                    'password_confirmation' => null,
+                ],
+            ],
+            'password_is_empty' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password'              => '',
+                    'password_confirmation' => '',
+                ],
+            ],
+            'password_has_wrong_data_type' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password'              => 123,
+                    'password_confirmation' => 123,
+                ],
+            ],
+            'password_is_too_long' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password'              => str_repeat('a', 256),
+                    'password_confirmation' => str_repeat('a', 256),
+                ],
+            ],
+            'password_confirmation_is_missing' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                ],
+            ],
+            'password_confirmation_is_different_form_password' => [
+                'invalid' => 'password',
+                'input' => [
+                    'name'                  => 'New User',
+                    'email'                 => 'user@example.com',
+                    'password'              => 'password',
+                    'password_confirmation' => 'password_confirmation',
+                ],
+            ],
         ];
     }
 }
