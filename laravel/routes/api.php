7d5d8c78d86716d54c781df9b35f1f3e24e1b185
mustakrakishe <mustakrkish@gmail.com>
2024-03-28 21:53:17 +0200

    Add names to api routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -10,25 +10,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest:sanctum')->group(function () {
     Route::prefix('auth')->group(function () {
-        Route::post('login', [LoginController::class, 'login']);
-        Route::prefix('password/reset')->group(function () {
-            Route::post('send', [ResetPasswordController::class, 'sendLink'])->middleware(TrustedHost::class);
-            Route::post('', [ResetPasswordController::class, 'reset']);
-        });
-
         Route::post('register', [RegisterController::class, 'register'])
-            ->middleware(TrustedHost::class);
+            ->middleware(TrustedHost::class)
+            ->name('api.register');
 
         Route::get('email/verify/{id}/{hash}', [RegisterController::class, 'verify'])
             ->middleware('signed')
             ->name('api.verification.verify');
+
+        Route::post('login', [LoginController::class, 'login'])->name('api.login');
+        Route::prefix('password/reset')->group(function () {
+            Route::post('send', [ResetPasswordController::class, 'sendLink'])->middleware(TrustedHost::class)->name('api.password.reset.send');
+            Route::post('', [ResetPasswordController::class, 'reset'])->name('api.password.reset');
+        });
     });
 });
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('auth')->group(function () {
-        Route::post('logout', [LogoutController::class, 'logout']);
+        Route::post('logout', [LogoutController::class, 'logout'])
+            ->name('api.logout');
     });
 
-    Route::apiResource('products', ProductController::class);
+    Route::apiResource('products', ProductController::class, [
+        'names' => [
+            'index'   => 'api.products.index',
+            'store'   => 'api.products.store',
+            'show'    => 'api.products.show',
+            'update'  => 'api.products.update',
+            'destroy' => 'api.products.destroy',
+        ],
+    ]);
 });
