f62fb83b2de171d82a1a925ad411d2c7e9d06d50
mustakrakishe <mustakrkish@gmail.com>
2024-03-29 11:27:14 +0200

    Fix submit buttn name on reset password page

--- laravel/resources/views/auth/reset-password.blade.php
+++ laravel/resources/views/auth/reset-password.blade.php
@@ -22,7 +22,7 @@
             </div>
 
             <div>
-                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
+                <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Reset password</button>
             </div>
         </form>
     </div>
