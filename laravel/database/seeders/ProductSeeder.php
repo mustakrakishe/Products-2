62c9fa4e86d82b544491ef15f62c9fa5e2e3e4aa
mustakrakishe <mustakrkish@gmail.com>
2024-03-29 12:50:17 +0200

    Fix ProductSeeder class name

--- laravel/database/seeders/ProductSeeder.php
+++ laravel/database/seeders/ProductSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
      * Seed the application's database.
