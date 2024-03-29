06a9d2469863e7422d2a1aa7243bfb08c469c7ee
mustakrakishe <mustakrkish@gmail.com>
2024-03-29 12:31:38 +0200

    Leavecurrency factory only in the database seeder

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Currency;
-use App\Models\Product;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,14 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
         Currency::factory(3)->create();
-        Product::factory(10)->create();
     }
 }
